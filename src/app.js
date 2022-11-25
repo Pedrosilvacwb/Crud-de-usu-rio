7d64ce6b406502da641493296f3a134df0c2474a
Pedro Silva <pedrosilvacwb@gmail.com>
2022-11-25 14:50:21 -0300

    feat: add feat list users + edit Users

--- src/app.js
+++ src/app.js
@@ -31,6 +31,48 @@ const findUserMidleware = (req, res, next) => {
   return next();
 };
 
+const validateTokenMidleWare = (req, res, next) => {
+  const authToken = req.headers.authorization;
+
+  if (!authToken) {
+    return res.status(401).json({ message: 'Missing Token' });
+  }
+  const token = authToken.split(' ')[1];
+
+  return jwt.verify(token, 'SECRET_KEY', (error, decode) => {
+    if (error) {
+      return res.status(401).json({ message: error.message });
+    }
+    const user = users.find((u) => u.email === decode.email);
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid Token' });
+    }
+    req.user = user;
+    return next();
+  });
+};
+
+const validateUserAdminMidleware = (req, res, next) => {
+  if (!req.user.isAdm) {
+    return res.status(403).json({ message: 'Unauthorized' });
+  }
+
+  return next();
+};
+
+const validateUserAdminEditMidleware = (req, res, next) => {
+  const paramsId = req.params.id;
+  const user = users.find((u) => u.uuid === paramsId);
+  const sameUsers = paramsId === req.user.uuid;
+
+  if (!req.user.isAdm && !sameUsers) {
+    return res.status(403).json({ message: 'Unauthorized' });
+  }
+
+  return next();
+};
+
 // Services
 const createUserService = async (data) => {
   const hashedPassword = await hash(data.password, 10);
@@ -67,6 +109,39 @@ const loginUserService = async ({ email, password }) => {
   return [200, { token }];
 };
 
+const getUsersService = () => {
+  return [200, users];
+};
+
+const retrieveUserService = (req) => {
+  const { user } = req;
+
+  const userDisplay = { ...user };
+  delete userDisplay.password;
+
+  return [200, userDisplay];
+};
+
+const editUserService = (req) => {
+  const user = users.find((u) => u.uuid === req.params.id);
+
+  const newUser = { ...user, ...req.body, updatedOn: new Date() };
+  const index = users.findIndex((u) => u === user);
+  users.splice(index, 1, newUser);
+  const userDisplay = { ...newUser };
+  delete userDisplay.password;
+
+  return [200, userDisplay];
+};
+
+const deleteUserService = (id) => {
+  const user = users.find((u) => u.uuid === id);
+  const index = users.findIndex((u) => u === user);
+  users.splice(index, 1);
+
+  return [204];
+};
+
 // Controller
 const createUserController = async (req, res) => {
   const [status, data] = await createUserService(req.body);
@@ -80,9 +155,55 @@ const loginUserController = async (req, res) => {
   return res.status(status).json(data);
 };
 
+const getUsersController = (req, res) => {
+  const [status, data] = getUsersService();
+
+  return res.status(status).json(data);
+};
+
+const retrieveUserController = (req, res) => {
+  const [status, data] = retrieveUserService(req);
+
+  return res.status(status).json(data);
+};
+
+const editUserController = (req, res) => {
+  const [status, data] = editUserService(req);
+
+  return res.status(status).json(data);
+};
+
+const deleteUserController = (req, res) => {
+  const id = req.params.id;
+  const [status, data] = deleteUserService(id);
+
+  return res.status(status).json(data);
+};
 // Routes
 app.post('/users', verifyUserExistsMidleWare, createUserController);
 app.post('/login', findUserMidleware, loginUserController);
+
+app.get(
+  '/users',
+  validateTokenMidleWare,
+  validateUserAdminMidleware,
+  getUsersController,
+);
+app.get('/users/profile', validateTokenMidleWare, retrieveUserController);
+
+app.patch(
+  '/users/:id',
+  validateTokenMidleWare,
+  validateUserAdminEditMidleware,
+  editUserController,
+);
+
+app.delete(
+  '/users/:id',
+  validateTokenMidleWare,
+  validateUserAdminEditMidleware,
+  deleteUserController,
+);
 
 app.listen(port, () => {
   console.log(`Rodando na porta ${port}`);
